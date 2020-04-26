9751114eaf5bb46f1e8dc745c8310bafb26c58ef
platinumtea <platinumtea7@gmail.com>
2020-04-26 17:38:19 -0400

    Added dynamic allocation and size constant

--- TicTacToe/Board.cpp
+++ TicTacToe/Board.cpp
@@ -2,85 +2,77 @@
 #include <stdio.h>
 
 Board::Board() {
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			boardStatus[i][j] = Piece::NONE;
+	board = new Piece*[BOARD_SIZE];
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		board[i] = new Piece[BOARD_SIZE];
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			board[i][j] = Piece::NONE;
 		}
 	}
 }
 
+Board::~Board() {
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		delete[] board[i];
+	}
+	delete[] board;
+}
+
 bool Board::move(Piece p, int x, int y) {
-	if (boardStatus[x][y] != Piece::NONE) {
+	if (board[x][y] != Piece::NONE) {
 		return false;
 	} else {
-		boardStatus[x][y] = p;
-		printf("[%d][%d]\n", x, y);
-		//printBoard();
+		board[x][y] = p;
 		return true;
 	}
 }
 
 Piece Board::getPiece(int x, int y) {
-	return boardStatus[x][y];
+	return board[x][y];
 }
 
 // Returns NONE if no one has won yet, otherwise returns the piece that won
 Piece Board::checkWin() {
 	// Check horizontal
-	for (int i = 0; i < 3; i++) {
-		if (boardStatus[i][0] != Piece::NONE && boardStatus[i][0] == boardStatus[i][1] && boardStatus[i][0] == boardStatus[i][2]) {
-			return boardStatus[i][0];
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		if (board[i][0] != Piece::NONE && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+			return board[i][0];
 		}
 	}
 
 	// Check vertical
-	for (int i = 0; i < 3; i++) {
-		if (boardStatus[0][i] != Piece::NONE && boardStatus[0][i] == boardStatus[1][i] && boardStatus[0][i] == boardStatus[2][i]) {
-			return boardStatus[0][i];
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		if (board[0][i] != Piece::NONE && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+			return board[0][i];
 		}
 	}
 
 	// Check diagonals
-	if (boardStatus[0][0] != Piece::NONE && boardStatus[0][0] == boardStatus[1][1] && boardStatus[0][0] == boardStatus[2][2]) {
-		return boardStatus[0][0];
+	if (board[0][0] != Piece::NONE && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+		return board[0][0];
 	}
-	if (boardStatus[0][2] != Piece::NONE && boardStatus[0][2] == boardStatus[1][1] && boardStatus[0][2] == boardStatus[2][0]) {
-		return boardStatus[0][2];
+	if (board[0][2] != Piece::NONE && board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+		return board[0][2];
 	}
 
 	return Piece::NONE;
 }
 
 void Board::reset() {
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			boardStatus[i][j] = Piece::NONE;
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			board[i][j] = Piece::NONE;
 		}
 	}
 }
 
 bool Board::isFull() {
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (boardStatus[i][j] == Piece::NONE) {
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (board[i][j] == Piece::NONE) {
 				return false;
 			}
 		}
 	}
 	return true;
-}
-
-void Board::printBoard() {
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (boardStatus[i][j] == Piece::X) {
-				printf(" X ");
-			} else if (boardStatus[i][j] == Piece::O) {
-				printf(" O ");
-			} else {
-				printf(" . ");
-			}
-		}
-		printf("\n");
-	}
 }
