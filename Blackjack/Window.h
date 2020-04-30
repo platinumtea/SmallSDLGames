f418da8e3655199f2f844f71c0c7f94dbac38b3e
platinumtea <platinumtea7@gmail.com>
2020-04-30 14:32:50 -0400

    Added extra rendering methods and renderer

--- Blackjack/Window.h
+++ Blackjack/Window.h
@@ -1,18 +1,38 @@
 #pragma once
 #include <SDL.h>
+#include <SDL_image.h>
 
 class Window {
 public:
+	// Con/De-structor
 	Window(const char* title,
 		int x, int y, int w,
 		int h, Uint32 flags);
 	~Window();
 
+	// Checks if construction was successful
 	bool successful();
 
-	void blit(SDL_Surface* src, const SDL_Rect* srcrect,
-		SDL_Surface* dst, SDL_Rect* dstrect);
+	// Rendering functions
+	void clear();
+	void copy(SDL_Texture* texture,
+		const SDL_Rect* srcrect,
+		const SDL_Rect* dstrect);
+	void copy(SDL_Texture* texture,
+		const SDL_Rect* srcrect,
+		int x, int y, int w, int h);
+	void update();
+
+	// Renderer-specfic functions
+	SDL_Texture* loadTexture(const char* path);
+	SDL_Texture* convertToTexture(SDL_Surface* surface);
+
+	void setIcon(const char* iconPath);
+
+	SDL_Surface* getWindowSurface();
+	SDL_Renderer* getWindowRenderer();
 private:
 	SDL_Window* m_window;
 	SDL_Surface* m_windowSurface;
+	SDL_Renderer* m_renderer;
 };
