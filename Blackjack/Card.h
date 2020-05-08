c32c68a74dcb15722a81bf5d1b4fb912046806cc
platinumtea <platinumtea7@gmail.com>
2020-05-08 13:26:39 -0400

    Add helper functions

--- Blackjack/Card.h
+++ Blackjack/Card.h
@@ -1,11 +1,23 @@
 #pragma once
+#include <SDL_rect.h>
+#include <vector>
+
 class Card {
 public:
 	Card(char suit, int value);
+
+	int getValue();
+
+	static bool splittable(std::vector<Card> pair);
+
+	SDL_Rect getClip();
 private:
 	// H for hearts, D for diamonds, S for spades, C for clubs
 	char m_suit;
+
+	// Note: C14 is red back and S14 is blue back
 	// 11 for jack, 12 for queen, 13 for king, 1 for ace
 	int m_value;
+
 };
 
